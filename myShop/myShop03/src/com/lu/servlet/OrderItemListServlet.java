209f723885440074b992b34370238ca077b6679f
jianlilu <1210195656@qq.com>
2017-06-13 11:39:09 +0800

    add myshop file

---
+++ myShop/myShop03/src/com/lu/servlet/OrderItemListServlet.java
@@ -0,0 +1,17 @@
+package com.lu.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OrderItemListServlet extends HttpServlet
+{
+ 
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("listOrderItem.jsp").forward(request, response);
+    }
+}
